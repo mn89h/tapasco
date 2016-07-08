5a1f99451060ccc762eb7f0ec88a9486f8fae6f6
Jens Korinth <jkorinth@gmx.net>
2016-07-08 15:33:36 +0200

    DecoupledDataSource module
    * testing utility: provides fixed data via Decoupled
    * configurable with/without wrap-around
    * unit tests

---
+++ build.sbt
@@ -0,0 +1,14 @@
+scalaVersion := "2.11.7"
+
+libraryDependencies += "edu.berkeley.cs" %% "chisel" % "latest.release"
+
+libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" %"test"
+
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.5.4"
+
+run in Compile <<= run in Compile map { (file) =>
+  "packaging/package.py ip/VgaTimingController.json" !
+}
+
---
+++ src/main/scala/DecoupledDataSource.scala
@@ -0,0 +1,37 @@
+import Chisel._
+
+/**
+ * Interface for DecoupledDataSource.
+ **/
+class DecoupledDataSourceIO[T <: Data](gen: T) extends Bundle {
+  val out = Decoupled(gen.cloneType)
+}
+
+/**
+ * Data source providing fixed data via Decoupled interface.
+ * Provides the data given via Decoupled handshakes; if repeat
+ * is true, data is wrapped around.
+ * @param gen Type.
+ * @param size Total number of elements.
+ * @param data Function providing data for each index
+ * @param repeat If true, will always have data via wrap-around,
+                 otherwise valid will go low after data was
+		 consumed.
+ **/
+class DecoupledDataSource[T <: Data](gen: T, val size : Int, data: (Int) => T, val repeat: Boolean = true) extends Module {
+  val ds  = for (i <- 0 until size) yield data(i) // evaluate data to array
+  val io  = new DecoupledDataSourceIO(gen) // interface
+  val i   = Reg(UInt(width = log2Up(if (repeat) size else size + 1))) // index
+  val rom = Vec.tabulate(size)(n => ds(n)) // ROM with data
+  io.out.bits  := rom(i) // current index data
+  io.out.valid := i < UInt(size) // valid until exceeded
+  when (reset) {
+    i := UInt(0)
+  }
+  .otherwise {
+    if (repeat)
+      when (io.out.ready && io.out.valid) { i := Mux(i < UInt(size - 1), i + UInt(1), UInt(0)) }
+    else
+      when (io.out.ready && io.out.valid && i < UInt(size)) { i := i + UInt(1) }
+  }
+}
