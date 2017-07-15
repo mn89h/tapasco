8a7425878a4242770946976dda87d4658c091814
Jens Korinth <jkorinth@gmx.net>
2017-07-15 18:42:23 +0200

    Squashed 'miscutils/' changes from 6d4e97b..6b570c6
    6b570c6 Fix bug in DecoupledDataSourceSuite
    dee2777 Update README.md
    ad4e6f3 README.md edited online with Bitbucket
    3528414 Update to Chisel 3.0 (SNAPSHOT)
    
    git-subtree-dir: miscutils
    git-subtree-split: 6b570c6a26a7707719404beace67a289289c90ed

--- src/main/scala/SignalGenerator.scala
+++ src/main/scala/SignalGenerator.scala
@@ -1,5 +1,6 @@
 package chisel.miscutils
-import Chisel._
+import  chisel3._
+import  chisel3.util._
 
 sealed case class Signal(value: Boolean, periods: Int = 1) extends Ordered[Signal] {
   import scala.math.Ordered.orderingToOrdered
@@ -16,31 +17,31 @@ class SignalGenerator(signals: SignalGenerator.Waveform, useInputAsClock: Boolea
   require (signals.length > 0, "Waveform must not be empty.")
   require (signals map (_.periods > 1) reduce (_&&_),
       "All signals must have at least two clock cycles length.")
-  val io = new Bundle { val v = Bool(OUTPUT); val in = Bool(INPUT) }
-  val cnts_rom = Vec(signals map (n => UInt(n.periods - 1)))
-  val vals_rom = Vec(signals map (n => Bool(n.value)))
-  val cnt      = Reg(UInt(width = log2Up(signals.max.periods)))
-  val curr_idx = Reg(UInt(width = log2Up(signals.length)))
+  val io = IO(new Bundle { val v = Output(Bool()); val in = Input(Bool()) })
+  val cnts_rom = Vec(signals map (n => (n.periods - 1).U))
+  val vals_rom = Vec(signals map (n => (n.value).B))
+  val cnt      = Reg(UInt(log2Ceil(signals.max.periods).W))
+  val curr_idx = Reg(UInt(log2Ceil(signals.length).W))
   val vreg     = Reg(Bool())
 
   io.v := vreg
 
   when (reset) {
-    curr_idx := UInt(0)
+    curr_idx := 0.U
     cnt      := cnts_rom(0)
-    vreg     := UInt(0)
+    vreg     := 0.U
   }
   .otherwise {
     vreg := vals_rom(curr_idx)
     // trigger on either clock or pos input edge
-    when (if (useInputAsClock) io.in && !RegNext(io.in) else Bool(true)) {
-      when (cnt === UInt(0)) {
-        val next_idx = Mux(curr_idx < UInt(signals.length - 1), curr_idx + UInt(1), UInt(0))
+    when (if (useInputAsClock) io.in && !RegNext(io.in) else true.B) {
+      when (cnt === 0.U) {
+        val next_idx = Mux(curr_idx < (signals.length - 1).U, curr_idx + 1.U, 0.U)
         curr_idx    := next_idx
         cnt         := cnts_rom(next_idx)
       }
       .otherwise {
-        cnt := cnt - UInt(1)
+        cnt := cnt - 1.U
       }
     }
   }
