2685786a1ae7bb09a8cb85402954d3e51774aacc
Jens Korinth <jkorinth@gmx.net>
2017-08-30 10:53:20 +0200

    SlowQueue: remove direct reset references

--- src/test/scala/SlowQueue.scala
+++ src/test/scala/SlowQueue.scala
@@ -13,24 +13,19 @@ class SlowQueue(width: Int, val delay: Int = 10) extends Module {
   val waiting :: ready :: Nil = Enum(2)
   val state = RegInit(ready)
 
-  val wr = Reg(UInt(log2Ceil(delay).W))
+  val wr = RegInit(UInt(log2Ceil(delay).W), io.dly)
 
   io.deq.bits  := io.enq.bits
   io.enq.ready := io.deq.ready && state === ready
   io.deq.valid := io.enq.valid && state === ready
 
-  when (reset) {
-    state := ready
+  when (state === ready && io.enq.ready && io.deq.valid) {
+    state := waiting
+    wr    := io.dly
   }
-  .otherwise {
-    when (state === ready && io.enq.ready && io.deq.valid) {
-      state := waiting
-      wr    := io.dly
-    }
-    when (state === waiting) {
-      wr := wr - 1.U
-      when (wr === 0.U) { state := ready }
-    }
+  when (state === waiting) {
+    wr := wr - 1.U
+    when (wr === 0.U) { state := ready }
   }
 }
 
