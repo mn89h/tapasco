f93ae1228f265ecb0898b0c79ddcbbc5a7918063
Jens Korinth <jkorinth@gmx.net>
2017-12-13 18:48:52 +0100

    Simplify overlap logic in BitRange

--- src/main/scala/axi4lite/package.scala
+++ src/main/scala/axi4lite/package.scala
@@ -4,8 +4,7 @@ package object axi4lite {
   /** Tuple-type for bit ranges. */
   sealed case class BitRange(to: Int, from: Int) {
     require (to >= from && from >= 0, "BitRange: invalid range (%d, %d)".format(to, from))
-    def overlapsWith(other: BitRange): Boolean = (from <= other.from && to >= other.from) ||
-                                                 (from >  other.from && from <= other.to)
+    def overlapsWith(other: BitRange): Boolean = !(to < other.from || from > other.to)
   }
   /** Names for bit ranges. **/
   type BitfieldMap = Map[String, BitRange]
