944327d244eec36e08f273add9f0f412b16ad78e
Jens Korinth <jk@esa.cs.tu-darmstadt.de>
2018-01-03 09:42:06 +0100

    Remove Verilog implementation of Tapasco Status Core

--- common/ip/tapasco_status_1.2/hdl/gen.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-str = """          12'd%d: reg_data_out <= C_SLOT_KERNEL_ID_%d;
-          12'd%d: reg_data_out <= 32'hFFFFFFFF - C_SLOT_LOCAL_MEM_%d;"""
-
-for x in range(1,129):
-    print str % (240 + x * 16, x, 240 + x * 16 + 4, x)
