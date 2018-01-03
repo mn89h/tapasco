a68c751e7cb711443702f736e7e565ec01c5a9bc
Jens Korinth <jk@esa.cs.tu-darmstadt.de>
2018-01-03 15:40:04 +0100

    Automatically use libmpfr.so workaround on Ubuntu

--- setup.sh
+++ setup.sh
@@ -19,3 +19,12 @@ fi
 echo "TAPASCO_HOME=$TAPASCO_HOME"
 export PATH=$TAPASCO_HOME/bin:$PATH
 export MANPATH=$MANPATH:$TAPASCO_HOME/man
+if uname -a | grep -i ubuntu > /dev/null 2>&1; then
+	if find /usr/lib -name libmpfr.so > /dev/null 2>&1; then
+		export LD_PRELOAD=`find /usr/lib -name libmpfr.so 2>/dev/null`
+	else
+		echo "WARNING: awk in Ubuntu Linux is incompatible with Vivado's old libmpfr.so" >&2
+		echo "This can be fixed by pre-loading a new libmpfr.so, but none was found in /usr/lib." >&2
+		echo "If you run into problems (awk: symbols not found), please install libmpfr."
+	fi
+fi
