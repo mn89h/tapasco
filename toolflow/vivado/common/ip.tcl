7daed0af1c712fe7315dc93ec7044e4b12bcd6d7
Malte Nilges <malte.nilges@hotmail.de>
2020-01-27 13:38:12 +0100

    Correct router address width

--- toolflow/vivado/common/ip.tcl
+++ toolflow/vivado/common/ip.tcl
@@ -134,7 +134,7 @@ namespace eval ::tapasco::ip {
     BuildArchIfc::build $ADDRESS_WIDTH
     BuildPEIfc::build $ADDRESS_WIDTH
     BuildMemIfc::build $ADDRESS_WIDTH
-    BuildRouter::build $ADDRESS_WIDTH
+    BuildRouter::build $DATA_WIDTH
 
     update_ip_catalog
 
